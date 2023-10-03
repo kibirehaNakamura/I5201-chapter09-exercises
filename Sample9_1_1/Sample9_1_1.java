28c8f4c8f09a8b3917e0a39163bbb671c841ec15
Nakamura <kisy23013@kibireha.ac.jp>
2023-10-03 15:10:44 +0900

    Sample9_1_1.javaを追加

---
+++ Sample9_1_1/Sample9_1_1.java
@@ -0,0 +1,16 @@
+public class Sample9_1_1 {
+	public static void main(String[] args) {
+		int i = 10;
+		double d = 1.5;
+		String st = "文字列:";
+		System.out.println(st);
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(st + " " + i + " " + d);
+		System.out.println();
+		System.out.print(st);
+		System.out.print(" " + i);
+		System.out.print(" " + d);
+		System.out.println();
+	}
+}
